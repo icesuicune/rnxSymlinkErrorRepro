5d2c75fae60f79edb05faee4c8bc5421efc64f4f
icesuicune <icesuicune@users.noreply.github.com>
2021-09-06 17:57:36 -0700

    temp

--- MyApp/metro.config.js
+++ MyApp/metro.config.js
@@ -4,14 +4,20 @@
  *
  * @format
  */
+const {
+  makeMetroConfig,
+  // excludeExtraCopiesOf,
+  // exclusionList,
+} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+// const additionalExclusions = [excludeExtraCopiesOf('react')];
+// const blockList = exclusionList(additionalExclusions);
+
+module.exports = makeMetroConfig({
+  projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver.default(),
   },
-};
+  // blockList, // For Metro >= 0.60
+});
